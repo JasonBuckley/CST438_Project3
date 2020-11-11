69d36520925e8cdb2b4d11d2562a071583e37dd6
Joseph-Villegas <jovillegas@csumb.edu>
2020-11-10 20:33:31 -0800

    added book detail page (starter)

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -4,15 +4,14 @@ $(document).ready(function () {
     $("#search-res").hide();
 
     $("#search-btn").on("click", function() {
-       // $("#productContainer").empty();
         let input = $("#search-bar").val();
+
         $("#search-res").hide();
 
         if (input === "") {
             alert("empty");
             return;
         }
-
         
         let title = input.replace(/\s+/g,'+');
         console.log("Searching for: ", title);
@@ -24,13 +23,13 @@ $(document).ready(function () {
                 let books = result.docs;
                 console.log(books);
 
-                let count = 1;
+                let count = 0;
 
                 for (let i = 0; i < books.length; i++) {
                     if (books[i].title && books[i].isbn.length != 0) {
                         addToRow(books[i], i);
                         count++;
-                        if (count == 5) {
+                        if (count == 4) {
                             break;
                         }
                     }
@@ -56,9 +55,6 @@ function searchByTitle(title) {
 //http://covers.openlibrary.org/b/id/9646548-S.jpg
 //http://covers.openlibrary.org/b/isbn/0385472579-S.jpg
 
-
-
-// creates a frame that holds the info for a product.
 // function addToRow(book) {
 //     let title = book.title;
 //     let isbn = book.isbn[0];
@@ -85,8 +81,18 @@ function addToRow(book, index) {
     let isbn = book.isbn[0];
     let img = `http://covers.openlibrary.org/b/isbn/${isbn}-M.jpg`
 
-    $(`#featured-img-${index+1}`).attr({"src": img, height: "300", width: "100%"});
-    $(`#feat-prod-name-${index+1}`).html(`${title}`);
-    $(`#feat-prod-price-${index+1}`).html(`$${isbn}`);
+    $(`#book-img-${index+1}`).attr({"src": img, height: "300", width: "100%", isbn: isbn});
+    $(`#book-title-${index+1}`).html(`${title}`);
+    $(`#book-isbn-${index+1}`).html(`${isbn}`);
 }
 
+
+document.querySelectorAll("[id^='book-img-']")
+  .forEach(function(el) {
+    el.onclick = function() {
+      console.log(this.id)
+      let isbn = $(`#${this.id}`).attr("isbn");
+      console.log(isbn);
+      window.location.href = `/book/isbn/${isbn}`;
+    }
+  })
