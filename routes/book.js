9b98df56fb97ec675be92843fd8c31bea4327dc9
Jason Buckley <jbuckley@csumb.edu>
2020-11-15 21:07:38 -0800

    implemented adding genres to a specific book

--- routes/book.js
+++ routes/book.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 const mysql = require("mysql");
 const https = require("https");
+let GENRES;
+
+
 
 // gets the config settings for the db
 const sqlConfig = {
@@ -12,14 +15,17 @@ const sqlConfig = {
     database: process.env.SQL_DATABASE
 };
 
-router.get('/isbn/:isbn', function(req, res, next) {
+router.get('/isbn/:isbn', function (req, res, next) {
     const isbn = [req.params.isbn];
-    res.render('book', {isbn: isbn});
+    res.render('book', { isbn: isbn });
 });
 
 // creates a pool to handle query requests.
 const pool = mysql.createPool(sqlConfig);
 
+/**
+ * Renders a page with specific information for a book.
+ */
 router.get("/page", async function (req, res, next) {
     if (!req.query.bookId) {
         return res.redirect('/');
@@ -52,6 +58,9 @@ router.get("/page", async function (req, res, next) {
     return res.render('bookPage', { book: result });
 });
 
+/**
+ * Gets 25 or fewer books from the database.
+ */
 router.get("/", async function (req, res, next) {
     if (req.query.search) {
         next();
@@ -65,7 +74,6 @@ router.get("/", async function (req, res, next) {
             if (err) {
                 reject(err);
             } else {
-                console.log(results);
                 resolve(results);
             }
         });
@@ -74,6 +82,9 @@ router.get("/", async function (req, res, next) {
     return res.json(result);
 });
 
+/**
+ * Gets all books that contain the search key.
+ */
 router.get("/", async function (req, res, next) {
     let result = await new Promise((resolve, reject) => {
         const query = `SELECT * FROM Book WHERE name LIKE ${pool.escape('%' + req.query.search + '%')} LIMIT 25;`;
@@ -91,6 +102,9 @@ router.get("/", async function (req, res, next) {
     return res.json(result);
 });
 
+/**
+ * Adds a book, and its genres given a valid isbn number.
+ */
 router.post("/add", async function (req, res, next) {
     if (!req.body.isbn) {
         res.json({ success: false });
@@ -98,16 +112,41 @@ router.post("/add", async function (req, res, next) {
 
     const url = new URL("https://openlibrary.org/api/books?bibkeys=ISBN:" + req.body.isbn + "&jscmd=data&format=json");
 
-    let result = await request(url)
-        .then((data) => addBook(data))
+    let isValid = await checkISBN(req.body.isbn)
         .catch((err) => {
             console.log(err);
-            return -1;
+            return false;
         });
 
-    return res.json({ success: result > -1 });
+    let result;
+    if (isValid) {
+        result = await request(url)
+            .then((data) => addBook(data))
+            .catch((err) => {
+                console.log(err);
+                return -1;
+            });
+    } else {
+        result = -1;
+    }
+
+    res.json({ success: result.insertId > -1 });
+
+    if (result.insertId > -1) {
+        await getGenresFromSubjects(result.subjects)
+            .then((genres) => addGenresToBook(genres, result.insertId))
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    return;
 });
 
+/**
+ * Adds a book to the Book table in the db.
+ * @param {JSON} data
+ */
 function addBook(data) {
     return new Promise((resolve, reject) => {
         if (!data) {
@@ -117,22 +156,122 @@ function addBook(data) {
         let author = data.authors ? data.authors[0].name : "Unknown";
         let title = data.title;
         let coverImg = data.cover ? data.cover.medium : "None";
-        let isbn = data.identifiers.isbn_13 ? data.identifiers.isbn_13[0] : data.identifiers.isbn_10[0];
+        let isbn13 = data.identifiers.isbn_13 ? data.identifiers.isbn_13[0] : "";
+        let isbn10 = data.identifiers.isbn_10 ? data.identifiers.isbn_10[0] : "";
         let publisher = data.publishers[0] ? data.publishers[0].name : "Unknown";
+        let subjects = data.subjects;
 
-        const query = 'INSERT INTO Book VALUES(null, ?, ?, ?, ?, ?);';
-        const values = [title, author, coverImg, isbn, publisher];
+        const query = 'INSERT INTO Book VALUES(null, ?, ?, ?, ?, ?, ?);';
+        const values = [title, author, coverImg, isbn13, isbn10, publisher];
 
         pool.query(query, values, (err, results) => {
             if (err) {
                 reject(err);
             } else {
-                resolve(results.insertId);
+                resolve({ insertId: results.insertId, subjects: subjects });
             }
         });
     });
 }
 
+/**
+ * This method takes in an array of subjects and scans for key words that indicate genres for a given book.
+ * @param {Array} subjects
+ * @returns Promise containing genre ids
+ */
+async function getGenresFromSubjects(subjects) {
+    if (!GENRES) {
+        GENRES = await getAllGenres();
+    }
+
+    return new Promise((resolve, reject) => {
+        let bookGenres = [];
+
+        if (subjects) {
+            // searches through a max of 25 subjects.
+            for (var i = 0; i < subjects.length && i < 25; i++) {
+                for (var j = 0; j < GENRES.length; j++) {
+                    if (subjects[i].name.toLowerCase().includes(GENRES[j].genre.toLocaleLowerCase()) && bookGenres.indexOf(GENRES[j].genreId) < 0) {
+                        bookGenres.push(GENRES[j].genreId);
+                    }
+                }
+            }
+        }
+
+        resolve(bookGenres);
+    });
+}
+
+/**
+ * Gets all genres from the db.
+ */
+function getAllGenres() {
+    return new Promise((resolve, reject) => {
+        const query = 'SELECT * FROM Genre LIMIT 100';
+
+        pool.query(query, (err, results) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+}
+
+/**
+ * Adds genres to the corresponding book.
+ * @param {array} genres 
+ * @param {int} bookId
+ */
+function addGenresToBook(genres, bookId) {
+    return new Promise((resolve, reject) => {
+        const tokens = new Array(genres.length).fill(`(?,${bookId})`).join(',');
+        const query = 'INSERT INTO Book_Genres (genreId, bookId) VALUES ' + tokens + ';';
+        const values = genres;
+
+        pool.query(query, values, (err, results) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+}
+
+/**
+ * checks if a isbn is valid and not already in the db.
+ * @param {string} isbn
+ */
+function checkISBN(isbn) {
+    return new Promise((resolve, reject) => {
+        if (isbn && (isbn.length < 10 || isbn.length > 13)) {
+            reject("Invalid ISBN");
+        } else {
+            const query = 'SELECT * FROM Book WHERE ISBN10 = ? OR ISBN13 = ? LIMIT 1;'
+            const values = [isbn, isbn];
+
+            pool.query(query, values, (err, results) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    if (Array.isArray(results) && results.length > 0 && results[0].bookId > -1) {
+                        reject("Book is already in Database!");
+                    } else {
+                        resolve(true);
+                    }
+                }
+            });
+        }
+    });
+}
+
+/**
+ * Given a url it will attempt to make a get request to the corresponding server.
+ * @param {URL} url
+ * @returns Promise containing json of data returned from the corresponding server.
+ */
 function request(url) {
     return new Promise((resolve, reject) => {
         let req = https.request(url, (res) => {
@@ -152,7 +291,6 @@ function request(url) {
                     } catch (e) {
                         reject('Error parsing JSON!');
                     }
-
                 }
             });
 
