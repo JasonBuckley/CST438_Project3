8f664947c081e8c2abb387e68a85058d5502f7af
XYIAN <kdilbeck@csumb.edu>
2020-11-17 07:56:45 +0000

    view completed, needs backend

--- public/javascripts/review.js
+++ public/javascripts/review.js
@@ -4,11 +4,12 @@ $(document).ready(function () {
 var feild = document.querySelector('textarea');
 var backUp = feild.getAttribute('placeholder');
 // var btn = document.querySelector('.btn');
+
+
 feild.onfocus = function(){
   this.setAttribute('placeholder', '');
   this.style.borderColor = '#333';
 //   btn.style.display = 'block';
-  
 };
 
 feild.onblur = function(){
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,9 +1,38 @@
 var express = require('express');
 const router = express.Router();
+const mysql = require('mysql');
 
+// gets the config settings for the db
+const sqlConfig = {
+    user: process.env.SQL_USERNAME,
+    password: process.env.SQL_PASSWORD,
+    host: process.env.SQL_HOST,
+    port: process.env.SQL_PORT,
+    database: process.env.SQL_DATABASE
+};
 
+// creates a pool to handle query requests.
+const pool = mysql.createPool(sqlConfig);
 router.get('/', function(req, res, next) {
+    
+    const q ='SELECT * from reviews';
+
+    pool.query(q, function (err, res) {
+    if (err) throw err;
+    console.log("Result: " + res);
+  });
+  
     res.render('reviews');
 });
 
+router.post("/add", function(req, res){
+    //const q ='INSERT INTO reviews VALUES (';
+    //const endq = ')';
+    req.body.reviewSubmit;
+    console.log(req.body);
+
+});
+
+
 module.exports = router;
+
